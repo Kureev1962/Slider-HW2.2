1a8bc579a4faae4e3b932a7433100daaeeafb43d
Gennady Kureev <gennady008@gmail.com>
2020-05-25 19:26:33 +0300

    Добавил экшен

--- Slider 2.2/ViewController.swift
+++ Slider 2.2/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var preview: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    func updateViewBackground() {
+        self.preview.backgroundColor = UIColor.init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: CGFloat(1)
+        )
+    }
+    
+    @IBAction func onBlueSliderChange() {
+       blueLabel.text = "Blue: \(String(format: "%.2f", blueSlider.value))"
+        updateViewBackground()
+    }
+    @IBAction func onGreenSliderChange() {
+        greenLabel.text = "Green: \(String(format: "%.2f", greenSlider.value))"
+        updateViewBackground()
+    }
+    @IBAction func onRedSliderChange() {
+        redLabel.text = "Red: \(String(format: "%.2f", redSlider.value))"
+        updateViewBackground()
+    }
 }
 
