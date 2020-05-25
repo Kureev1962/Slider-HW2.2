f2895297c9a8530d4f66672901ca1d7ac9e819a0
Gennady Kureev <gennady008@gmail.com>
2020-05-25 15:01:32 +0300

    Initial Commit

---
+++ Slider 2.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider 2.2
+//
+//  Created by Kureev on 25.05.2020.
+//  Copyright © 2020 Kureev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
